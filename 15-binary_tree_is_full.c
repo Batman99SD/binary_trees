19250328c2a060db087d9fb44c5e462ed7a34118
Batman99SD <azozkrg1@gmail.com>
2024-02-29 15:05:40 -0800

    16

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -5,7 +5,8 @@
  * @tree: A pointer to the root node of the tree to check.
  * Return: 0 or 1
  */
-int binary_tree_is_full(const binary_tree_t *tree) {
+int binary_tree_is_full(const binary_tree_t *tree)
+{
 	if (tree == NULL)
 		return (0);
 
